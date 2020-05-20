9f6bf0170890c8e1aa5f018d3f7c57dab4408e6d
Duarte Cardoso, Igor <igor.duarte.cardoso@intel.com>
2020-05-20 15:07:28 -0700

    cpconf.sh able to backup pretty much any file now

--- bash/cpconf.sh
+++ bash/cpconf.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-filelist=$1
+operation=$1
+filelist=$2
 
-mkdir _root 2>/dev/null
 
-while IFS= read -r filename
-do
-  dir=`dirname $filename`
-  mkdir -p _root/$dir && cp -R $filename "$_"
-  echo "$filename"
-done < "$filelist"
+backup() {
+  mkdir _root 2>/dev/null
 
+  while IFS= read -r filename
+  do
+    dir=`dirname "$filename"`
+    mkdir -p "_root$dir"
+    cp -pR ${filename} "$_"
+    echo "$filename"
+  done < "$filelist"
+}
+
+
+restore() {
+  pwd
+}
+
+case $operation in
+  backup )
+	  echo Backing up...
+	  backup
+    ;;
+  restore )
+	  echo Restoring...
+	  restore
+    ;;
+  * )
+	  echo "Usage: cpconf backup <file with filelist>"
+	  echo "       cpconf restore <backup root dir> <true root dir>"
+    ;;
+esac
